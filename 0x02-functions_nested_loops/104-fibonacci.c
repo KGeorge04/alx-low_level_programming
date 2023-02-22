227b74a2f5594f8bf612e3568d03f3458685e678
KGeorge04 <george.mwaro@yahoo.com>
2023-02-22 00:44:48 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - fibonaci<3
- *
- * Purpose - no hardcode
- *
- * Return: (Success)
+ * main - Finds and prints the first 98 Fibonacci numbers, starting with
+ * 1 and 2, separated by a comma followed by a space
+ * Return: Always 0
  */
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_part1, fib1_part2, fib2_part1, fit2_part2;
+	unsigned long part1, part2;
 
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
+	for (count = 0; count < 92; count++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		sum = fib1 + fib2;
+		printf("%lu,", sum);
+		fib1 = fib2;
+		fib2 = sum;
 	}
 
-	bef1 = (bef / 1);
-	bef2 = (bef % 1);
-	aft1 = (aft / 1);
-	aft2 = (aft % 1);
+	fib1_part1 = fib1 / 10000000000;
+	fib2_part1 = fib2 / 10000000000;
+	fib1_part2 = fib1 % 10000000000;
+	fib2_part2 = fib2 % 10000000000;
 
-	for (i = 92; i < 99, ++i)
+	for (count = 93; count < 99; count++)
 	{
-		printf(", %lu", aft1 + (aft2 / 1));
-		printf("%lu", aft2 % 1);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		part1 = fib1_part1 + fib2_part1;
+		part2 = fib1_part2 + fib2_part2;
+		if (fib1_part2 + fib2_part2 > 9999999999)
+		{
+			part1 += 1;
+			part2 %= 10000000000;
+		}
+	printf("%lu%lu", part1, part2);
+	if (count != 98)
+		printf(", ");
+	fib1_part1 = fib2_part1;
+	fib1_part2 = fib2_part2;
+	fib1_part1 = part1;
+	fib2_part2 = part2;
 	}
 	printf("\n");
 	return (0);
+}
